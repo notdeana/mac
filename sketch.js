8d4dd8019e00262988835a8e0ab471825842166b
おれわころせ <32080425+notdeana@users.noreply.github.com>
2021-04-11 23:07:00 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
+let array = [];
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(2560, 1440);
+  background(10);
+  strokeWeight(7)
 }
 
 function draw() {
-  background(220);
+  //console.log(mouseIsPressed);
+  if (mouseIsPressed == true) {
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+  }
+}
+
+function keyTyped() {
+  //console.log(`key ${key} is being typed`)
+  //console.log(`key ${key} is being typed`)
+  if (key === 's') {
+    //save image
+    saveCanvas('fileName', 'png');
+  } else if (key === 'd'){
+    for(let i = 0; i < array.length - 1; i++){
+          line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      //console.log(i);
+    }
+
+    //line(array[1][0], array[1][1]), array[2][0], array[2][1]);
+  }
+    //display image
+    //console.log(array);
+    //console.log(array[0]);
+    //console.log(array[0][1]);
+  return false;
 }
