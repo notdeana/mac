72e839063764f3fdf6ed719473f2be01b7bd151c
おれわころせ <32080425+notdeana@users.noreply.github.com>
2021-04-12 21:58:53 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,31 @@
 let array = [];
+let backgroundColor = 200;
+
 function setup() {
-  createCanvas(2560, 1440);
-  background(10);
+  createCanvas(windowWidth, windowHeight);
+  background(500);
   strokeWeight(7)
+  noFill();
 }
 
 function draw() {
   //console.log(mouseIsPressed);
   if (mouseIsPressed == true) {
+    // clear();
+    //ellipse(mouseX, mouseY, 100);
+    background(backgroundColor);
+    backgroundColor -= 7;
     stroke(map(mouseX, 0, 600, 0, 255, true))
     //line(mouseX, mouseY, pmouseX, pmouseY);
     array.push([mouseX, mouseY]);
-    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    //beginShape();
+    //for(let i = 0; i < array.length; i++){
+    //line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+    //console.log(i);
+    //curveVertex(array[i][0], array[i][1])
+    //}
+    //endShape();
+    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
   }
 }
 
@@ -21,17 +35,26 @@ function keyTyped() {
   if (key === 's') {
     //save image
     saveCanvas('fileName', 'png');
-  } else if (key === 'd'){
-    for(let i = 0; i < array.length - 1; i++){
-          line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+  } else if (key === 'd') {
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
+      //line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
       //console.log(i);
+      curveVertex(array[i][0], array[i][1])
     }
-
+    endShape();
     //line(array[1][0], array[1][1]), array[2][0], array[2][1]);
   }
-    //display image
-    //console.log(array);
-    //console.log(array[0]);
-    //console.log(array[0][1]);
+  //display image
+  //console.log(array);
+  //console.log(array[0]);
+  //console.log(array[0][1]);
   return false;
+}
+
+function mousePressed() {
+  //console.log("mouse pressed function");
+  array = [];
+  backgroundColor = 255;
+  //console.log(array)
 }
